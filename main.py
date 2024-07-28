8330846f2019cce98bdef9e2fad4e5954cf801b3
RYAN MATHEWS <mathewsryan2@gmail.com>
2024-07-28 15:44:07 +0100

    Finished PasswordCheck function, added some comments and added the new empty function "PasswordGenerator"

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 import os
 
-def PasswordCheck(Length, UpperC, LowerC, SpecialC, Name, Email, NumC, File):
+def PasswordCheck(Length, UpperC, LowerC, SpecialC, FName, LName, Email, NumC, Dict_File):
     #setting to False and than to true to keep the loop running when something is wrong.
      valid = False
      while(not valid):
@@ -56,14 +56,24 @@ def PasswordCheck(Length, UpperC, LowerC, SpecialC, Name, Email, NumC, File):
             print("Not enough numbers. ")
             valid = False 
 
-        if password.find(File) != -1:
-           print("Bad") 
+#If password contains a word over 5 characters in dictionary it is invalid.
+        for word in Dict_File:
+            word = word.strip()
+            if (len(word) > 5):
+                if word in password:
+                    print("can't have dictionary word. ")
+                    valid = False
+                
+            
         #need to fix to where all caps doesn't work either
-        if password.find(Name) != -1:
-            ("Name cannot be used in password. ")
+        if password.find(FName) != -1:
+            ("First name cannot be used in password. ")
             valid = False 
             print("Can't have users name. ")    
 
+        if password.find(LName) != -1:
+            ("Last name cannot be used in password. ")
+            valid == False
 
         if password.find(Email) != -1:
             print("Email cannot be used in password") 
@@ -77,11 +87,15 @@ def PasswordCheck(Length, UpperC, LowerC, SpecialC, Name, Email, NumC, File):
             print("Password is invalid, please try again. ")
              
 
+def PasswordGenerator():
+    print("Placeholder")
+
+
 #Set of characters acceptable to be special characters 
 SpecialList = ["!", "@", "#", "$", "%", "^", "&", "*"]
 
 #Opening the dictionary file to check prefixes
-File = open("Dictionary.txt", "r")
+Dict_File = open("Dictionary.txt", "r")
 
 
 #User input area to get all variables
@@ -90,14 +104,23 @@ UpperC = int(input("How many uppercase characters are required? "))
 LowerC = int(input("How many lowercase characters are required? "))
 SpecialC = int(input("How many special characters are required? "))
 NumC = int(input("How many numeric characters are required? "))
+
+
 #If no than any dictionary word above 5 characters is not allowed. 
 Dict = input("Are dictionary words allowed? (Y,N): ")
+if Dict == 'Y' or Dict == 'y':
+    Dict_File == " "
+
+#Getting name input if needed
 Name_Input= input("Is the users name allowed? (Y,N): ")
 if Name_Input == 'N' or Name_Input == 'n': 
-   Name = input("Please enter your name: ") 
+   FName = input("Please enter your first name:\n ") 
+   LName = input("Please enter your last name: ")
 else:
 #setting to an illegal character to pass to the function
     Name = " "
+
+#Getting email input if needed
 Email_Input = input("Is email allowed? (Y,N): ")
 if Email_Input == 'N' or Email_Input == 'n':
     Email = input("Please enter your email: ")
@@ -115,7 +138,7 @@ choice = input()
 #Calling PasswordCheck if 1 
 #calling generate password if 2
 if(choice == '1'):
-    PasswordCheck(Length, UpperC, LowerC, SpecialC, Name, Email, NumC, File)
+    PasswordCheck(Length, UpperC, LowerC, SpecialC, FName, LName, Email, NumC, Dict_File)
 elif(choice == '2'):
     ##generate password
    print("okay") 
